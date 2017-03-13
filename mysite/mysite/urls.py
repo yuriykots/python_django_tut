14c8c8df8b8f244034ea8eecf311aecec2c797e0
yuriykots <setingm@gmail.com>
2017-03-13 15:29:23 -0400

    Adding music app to Django project

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
-    url(r'^test/', include('polls.urls')),
+    url(r'^music/', include('music.urls')),
     url(r'^admin/', admin.site.urls),
 ]
