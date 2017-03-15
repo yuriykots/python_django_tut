b9e46a4a0b64fb1ffb9d48c9feae00e7eb45b598
yuriykots <setingm@gmail.com>
2017-03-14 22:18:05 -0400

    Using shortcuts to render intex.html template

--- mysite/music/views.py
+++ mysite/music/views.py
@@ -1,9 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Album
 
 # Create your views here.
 
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>Music section will be here</h1>")
+    #making a call to database and storing all objects in all_albums
+    all_albums = Album.objects.all()
+    context = {'all_albums': all_albums }
+    #behind a scene render() converts in in http response
+    return render(request, 'music/index.html', context)
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details on album ID  " + str(album_id)+ "</h2>")
