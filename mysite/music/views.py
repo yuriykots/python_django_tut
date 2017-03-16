62e11bf9dea3af336d2d3733585308dd13b0905c
yuriykots <setingm@gmail.com>
2017-03-16 18:12:30 -0400

    Changing function in our view to classes CreateView, UpdateView

--- mysite/music/views.py
+++ mysite/music/views.py
@@ -1,38 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from .models import Album, Song
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Album
 
-# Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
+    def get_queryset(self):
+        return Album.objects.all()
 
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
 
-def index(request):
-    #making a call to database and storing all objects in all_albums
-    all_albums = Album.objects.all()
-    #behind a scene render() converts in in http response
-    return render(request, 'music/index.html', {'all_albums': all_albums })
-
-def detail(request, album_id):
-    #using this shortcut instead of try / except loop
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album' : album})
-
-# receiving post method.
-# request.POST['song'] - will have a value of selected song.
-def favorite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk = request.POST['song'])
-# Printing request.POST in console
-        print(request.POST)
-#If there is an error/DoesNotExist trowing error
-    except (KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error_message': "You did not select a valid song",
-        })
-#If everything is good redirecting to detail view.
-    else:
-        selected_song.is_favorite = True
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album' : album})
+class AlbumCreate(CreateView):
+    model = Album
+    #what fields do you need.
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
