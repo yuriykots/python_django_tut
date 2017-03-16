e80829e999814959046a04520152d80ba7761e38
yuriykots <setingm@gmail.com>
2017-03-16 18:13:03 -0400

    Adding url to /music/album/add

--- mysite/music/urls.py
+++ mysite/music/urls.py
@@ -6,13 +6,23 @@ app_name = 'music'
 
 urlpatterns = [
     #/music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     #/music/<album_di>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail' ),
-
-    #/music/<album_di>/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite' ),
-
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail' ),
+    #/music/album/add
+    url(r'album/add/$',views.AlbumCreate.as_view(), name='album-add'),
 
 ]
+
+
+
+#
+#urlpatterns = [
+#    #/music/
+#    url(r'^$', views.index, name='index'),
+#
+#    #/music/<album_di>/
+#    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail' ),
+#
+#]
