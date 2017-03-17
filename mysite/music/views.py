26d6cd1fcad7dfe0e685b0298c4e9c40cb3256ad
yuriykots <setingm@gmail.com>
2017-03-17 17:51:24 -0400

    Adding AlbumUpdate and AlbumDelete Views

--- mysite/music/views.py
+++ mysite/music/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+
 from .models import Album
 
 class IndexView(generic.ListView):
@@ -15,5 +17,18 @@ class DetailView(generic.DetailView):
 
 class AlbumCreate(CreateView):
     model = Album
-    #what fields do you need.
+    # what fields do you need.
     fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    # what fields do you need.
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+    # success_url = "/music/"
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
+
+
+#{% url 'music:album-delete' album.id %}
