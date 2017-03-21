dad60476bb9a32eb9d7893241cd0890c959a5377
yuriykots <setingm@gmail.com>
2017-03-21 15:29:07 -0400

    Add '/register' url

--- mysite/music/urls.py
+++ mysite/music/urls.py
@@ -7,12 +7,14 @@ app_name = 'music'
 urlpatterns = [
     #/music/
     url(r'^$', views.IndexView.as_view(), name='index'),
+    #/music/
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
 
     #/music/<album_di>/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail' ),
     #/music/album/add
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
-
+ 
     #/music/album/2/
     url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
 
